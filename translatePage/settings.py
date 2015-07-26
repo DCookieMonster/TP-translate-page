2f9e7161d22bf3c7ac4c21dc81182a15656689b2
dor <amirdor@gmail.com>
2015-07-26 23:49:34 +0300

    herokuapp3.1

--- translatePage/settings.py
+++ translatePage/settings.py
@@ -130,6 +130,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 # STATIC_ROOT = 'staticfiles'
 # STATIC_URL = '/static/'
+STATIC_URL = '/static/'
 MEDIA_ROOT =  os.path.join(BASE_DIR,"static","media")
 STATIC_ROOT = os.path.join(BASE_DIR,"static","static-only")
 STATICFILES_DIRS = (
